4a3a49da702684efea359c0812129900fcf02920
NBMA0H-LBD <NBMA0H@neptun.gde.hu>
2023-11-30 17:06:10 +0100

    1

---
+++ Szalloda.py
@@ -0,0 +1,49 @@
+# 1. Osztályok Létrehozása
+
+
+from abc import ABC, abstractmethod
+
+class Szoba(ABC):
+    def __int__(self, szobaszam, ar):
+        self.szobaszam = szobaszam
+        self.ar = ar
+
+    def info(self):
+        pass
+
+class egyagyas_szoba(Szoba):
+    def info(self):
+        return f" Egyágyas szoba {self.szobaszam}, ár : {self.ar}"
+
+class Szalloda:
+    def __int__(self, nev):
+        self.nev = nev
+        self.szobak =[]
+
+    def add_szoba(self, szoba):
+        self.szobak.append(szoba)
+
+    def szobak_listaja(self):
+        for szoba in self.szobak:
+            print(szoba.info())
+
+class Foglalas:
+    def __int__(self,szoba, datum):
+        self.szoba = szoba
+        self.datum = datum
+
+    def info(self):
+        return f"Foglalás az {self.szoba.info()} szobára a {self.datum} napon"
+
+# 2. Foglalások kezelése
+
+class Hotel:
+    def __int__(self,nev):
+        self.nev = nev
+        self.szobak = []
+        self.foglalasok []
+
+    def add_szoba(self,szoba):
+        self.szobak.append(szoba)
+
+
