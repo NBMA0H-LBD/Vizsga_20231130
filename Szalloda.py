052b9eb22c2faf92fffd2da8f6cb571839d15ab1
NBMA0H-LBD <NBMA0H@neptun.gde.hu>
2023-11-30 18:08:29 +0100

    1

--- Szalloda.py
+++ Szalloda.py
@@ -79,6 +79,71 @@ class Foglalas:
         self.datum = datum
         self.ar = ar
 
-    def info(self):
-        return f"Foglalás {self.szoba.info()} szobára a {self.datum} dátumra. Ár: {self.ar}"
+    def informacio(self):
+        return f"Foglalás {self.szoba.informacio()} szobára a {self.datum} dátumra. Ár: {self.ar}"
+
+    def feltoltes_uj(szalloda):
+        szobak_szama = 3
+        foglalasok_szama = 5
+
+        for i in range(1, szobak_szama + 1):
+            szalloda.add_szoba(EgyagyasSzoba(szobaszam=i))
+            szalloda.add_szoba(KetagyasSzoba(szobaszam=szobak_szama + i))
+
+        for i in range(foglalasok_szama):
+                szalloda.foglalas(szobaszam=i % szobak_szama + 1,
+                                  datum=(datetime.now() + timedelta(days=i)).strftime('%Y-%m-%d'))
+
+    if __name__ == "__main":
+        hotel == Hotel(nev="Lábadi Zoltán")
+        feltoltes_uj(hotel)
+
+        while True:
+            print("\nVálassz műveletet:")
+            print("1. Foglalás")
+            print("2. Lemondás")
+            print("3. Foglalások listázása")
+            print("4. Kilépés")
+
+            valasztas = input("Válassz (1-4): ")
+
+            if valasztas == "1":
+                szobaszam = input("Add meg a szoba számát: ")
+                datum = input("Add meg a foglalás dátumát (YYYY-MM-DD): ")
+
+                try:
+                    datetime_obj = datetime.strptime(datum, '%Y-%m-%d')
+                    if datetime_obj < datetime.now():
+                        print("Érvénytelen dátum, csak jövőbeni foglalás lehetséges.")
+                        continue
+
+                    print(hotel.foglalas(szobaszam, datum))
+                except ValueError:
+                    print("Hibás dátumformátum. Kérlek használd az YYYY-MM-DD formátumot.")
+
+            elif valasztas == "2":
+                szobaszam = input("Add meg a szoba számát: ")
+                datum = input("Add meg a foglalás dátumát (YYYY-MM-DD): ")
+
+                try:
+                    datetime_obj = datetime.strptime(datum, '%Y-%m-%d')
+                    if datetime_obj < datetime.now():
+                        print("Érvénytelen dátum, csak jövőbeni lemondás lehetséges.")
+                        continue
+
+                    print(hotel.lemondas(szobaszam, datum))
+                except ValueError:
+                    print("Hibás dátumformátum. Kérlek használd az YYYY-MM-DD formátumot.")
+
+            elif valasztas == "3":
+                hotel.listaz_foglalasok()
+
+            elif valasztas == "4":
+                break
+
+            else:
+                print("Érvénytelen választás. Kérlek válassz 1-4 között.")
+
+
+
 
